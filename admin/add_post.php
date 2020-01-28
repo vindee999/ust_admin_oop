c849cf21e10460181b4f1efe5b04414e373b9025
vindee <vindee999@gmail.com>
2020-01-28 23:37:25 +0530

    created add post

--- admin/add_post.php
+++ admin/add_post.php
@@ -42,7 +42,7 @@
       <div class="container">
         <nav class="blog-nav">
           <a class="blog-nav-item active" href="index.php">Admin Panel</a>
-          <a class="blog-nav-item" href="add_post.php">Add Post</a>
+          <a class="blog-nav-item" href="">Add Post</a>
           <a class="blog-nav-item" href="">Add Category</a>
           <a class="blog-nav-item pull-right" href="localhost/ustad_admin_oop">View Log</a>
           <a class="blog-nav-item pull-right" href="logout.php">Logout</a>
@@ -55,40 +55,26 @@
       <div class="row">
 
         <div class="col-sm-12 blog-main">
+        <br>
+        <h2>Add New Post</h2><hr>
 
-          <table class="table table-striped">
-              <tr align="center">
-                <td colspan="4"><h1>Manage Posts</h1></td>
-              </tr>
-              <tr>
-                <th>Post ID</th>
-                <th>Post Title</th>
-                <th>Post Author</th>
-                <th>Post Date</th>
-              </tr>
-              <?php while($row = $posts->fetch_array()): ?>
-              <tr>
-                <td><?php echo $row['id'] ?></td>
-                <td><a href="edit.php?id=<?php echo $row['id'] ?>"><?php echo $row['title'] ?></a></td>
-                <td><?php echo $row['author'] ?></td>
-                <td><?php echo formate_date($row['date']) ?></td>
-              </tr>
-              <?php endwhile; ?>
-          </table>
-
-          <table class="table table-striped">
-              <tr align="center">
-                <td colspan="4"><h1>Manage Categories</h1></td>
-              </tr>
-              <tr>
-                <th>Category ID</th>
-                <th>Category Title</th>
-              </tr>
-              <?php while($row1 = $categories->fetch_array()): ?>
-              <tr>
-                <td><?php echo $row1['id'] ?></td>
-                <td><a href="edit_cat.php?id=<?php echo $row1['id'] ?>"><?php echo $row1['title'] ?></a></td>
-              </tr>
-              <?php endwhile; ?>
-          </table>
-
+        <form action="add_post" method="post">
+          <div class="form-group">
+            <label>Title</label>
+            <input type="text" name="title" class="form-control" placeholder="Enter Title">
+          </div>
+          <div class="form-group">
+            <label>Content</label>
+            <input type="text" name="content" class="form-control" placeholder="Enter Content">
+          </div>
+          <div class="form-group">
+            <label >Author</label>
+            <input type="text" name="author" class="form-control" placeholder="Enter Author">
+          </div>
+          <div class="form-group">
+            <label>Tags</label>
+            <input type="text" name="tags" class="form-control" placeholder="Enter Tags">
+          </div>
+          
+          <button type="submit" class="btn btn-success">Submit</button>
+        </form>
