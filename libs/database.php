f4c508354cc7b72f3a29bf34f4e20d9da37d8d06
vindee <vindee999@gmail.com>
2020-01-28 16:33:13 +0530

    tutorial 6 db queries

--- libs/database.php
+++ libs/database.php
@@ -14,8 +14,57 @@
 		}
 
 		private function connect(){
-			$con = new mysqli($this->dbHost,$this->dbUser,$this->dbPass,$this->dbName);
+			$this->link = new mysqli($this->dbHost,$this->dbUser,$this->dbPass,$this->dbName);
+
+			if(!$this->link){
+				$this->error =  "Connection Failed ".$this->link->connect_error;
+			}
 		}
+
+		public function select($query){
+			$result = $this->link->query($query);
+
+			if($result->num_rows > 0){
+				return $result;
+			}else{
+				return false;
+			}
+
+		}
+
+		public function insert($query){
+			$insert = $this->link->query($query);
+
+			if($insert){
+				header('Location: index.php?insert=Post Inserted');
+			}else{
+				echo "Did not insert";
+			}
+
+		}
+
+		public function update($query){
+			$update = $this->link->query($query);
+
+			if($update){
+				header('Location: index.php?insert=Post Updated');
+			}else{
+				echo "Did not update";
+			}
+
+		}
+
+		public function delete($query){
+			$delete = $this->link->query($query);
+
+			if($delete){
+				header('Location: index.php?insert=Post Deleted');
+			}else{
+				echo "Did not update";
+			}
+
+		}
+
 
 	}
 
