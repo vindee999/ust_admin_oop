fb3af936c4be4a73533552bcdad14135a6a7493d
vindee <vindee999@gmail.com>
2020-01-28 20:49:54 +0530

    tutorial 11 admin setup

---
+++ admin/includes/footer.php
@@ -0,0 +1,26 @@
+ 
+
+        </div><!-- /.blog-main -->
+
+
+
+      </div><!-- /.row -->
+
+    </div><!-- /.container -->
+
+    <footer class="blog-footer">
+      <p>Blog template built for <a href="https://getbootstrap.com/">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+      <p>
+        <a href="#">Back to top</a>
+      </p>
+    </footer>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="../bootstrap/bootstrap.js"></script>
+  </body>
+</html>
---
+++ admin/includes/header.php
@@ -0,0 +1,59 @@
+<?php 
+  include '../libs/database.php';
+  include '../libs/config.php';
+  include '../functions.php';
+
+  $db = new Database;
+
+  $query = "Select * from posts order by id desc";
+  $posts = $db->select($query);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+    <link rel="canonical" href="https://getbootstrap.com/docs/3.3/examples/blog/">
+
+    <title>Admin Panel</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../bootstrap/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="../bootstrap/custom.css" rel="stylesheet">
+
+    
+  </head>
+
+  <body>
+
+    <div class="blog-masthead">
+      <div class="container">
+        <nav class="blog-nav">
+          <a class="blog-nav-item active" href="index.php">Admin Panel</a>
+          <a class="blog-nav-item" href="">Add Post</a>
+          <a class="blog-nav-item" href="">Add Category</a>
+          <a class="blog-nav-item pull-right" href="localhost/ustad_admin_oop">View Log</a>
+          <a class="blog-nav-item pull-right" href="logout.php">Logout</a>
+        </nav>
+      </div>
+    </div>
+
+    <div class="container">
+
+      <div class="row">
+
+        <div class="col-sm-12 blog-main">
+
+          <table class="table table-striped">
+            
+          </table>
+
