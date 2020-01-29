7561053f81e312b217028667ee51b011e257565c
vindee <vindee999@gmail.com>
2020-01-29 13:29:31 +0530

    tutorial 19 login page

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,8 @@
 <?php 
 
+  if(!isset($_SESSION['email'])){
+  		header('Location:login.php');
+  }
   include 'includes/header.php';
   include 'includes/footer.php';
 
---
+++ admin/login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Simple Login Form</title>
+<meta charset="UTF-8" />
+<link rel="stylesheet" type="text/css" href="css/reset.css">
+<link rel="stylesheet" type="text/css" href="css/structure.css">
+</head>
+
+<body>
+<form class="box login">
+	<fieldset class="boxBody">
+	  <label>Email</label>
+	  <input type="text" name="email" tabindex="1" placeholder="Email" required>
+	  <label>Password</label>
+	  <input type="password" name="password" tabindex="2" placeholder="Password" required>
+	</fieldset>
+	<footer>
+	  <input type="submit" class="btnLogin" value="Login" tabindex="4">
+	</footer>
+</form>
+
+</body>
+</html>
